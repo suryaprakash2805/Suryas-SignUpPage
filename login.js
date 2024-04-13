7e029744f7267865926cabf2628f9cd02dd036e6
suryaprakash2805 <suryaprakashraj28@gmail.com>
2024-04-13 19:28:35 +0530

    final commit

--- login.js
+++ login.js
@@ -1,4 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {});
+
+const bgImage = new Image();
+bgImage.src = "background.jpg";
+bgImage.onload = function () {
+  document.body.style.backgroundImage = "url('background.jpg')";
+  loadHTMLContent();
+};
+
+function loadHTMLContent() {
+  const container = document.createElement("div");
+  container.classList.add("container");
+  container.innerHTML = `
+  <div class="form-box" id="form">
+  <h1 id="title">Login</h1>
+  <form id="loginForm">
+    <div class="input-field" id="namefield">
+      <i class="fa-solid fa-user"></i>
+      <input
+        type="text"
+        placeholder="Username"
+        id="username"
+        name="username"
+        autocomplete="off"
+      />
+    </div>
+    <div class="input-field">
+      <i class="fa-solid fa-lock"></i>
+      <input
+        type="password"
+        placeholder="Password"
+        id="password"
+        name="password"
+      />
+    </div>
+    <div class="btn-field">
+      <button type="button" id="loginBtn">Login</button>
+    </div>
+  </form>
+  <p class="Login">
+    Don't have an account? <a href="index.html"> Register</a>
+  </p>
+</div>
+            `;
+  document.body.appendChild(container);
+
   const loginBtn = document.getElementById("loginBtn");
 
   loginBtn.addEventListener("click", function (event) {
@@ -35,54 +80,4 @@ document.addEventListener("DOMContentLoaded", function () {
       alert("Please enter a valid username or password.");
     }
   });
-});
-
-const bgImage = new Image();
-bgImage.src = "background.jpg";
-bgImage.onload = function () {
-  // Once the image is loaded, add it to the body as the background
-  document.body.style.backgroundImage = "url('background.jpg')";
-  // Now load the rest of the HTML content
-  loadHTMLContent();
-};
-
-function loadHTMLContent() {
-  // Here you can add your HTML content dynamically
-  const container = document.createElement("div");
-  // container.className = "container";
-  // Add your HTML content here
-  container.classList.add("container");
-  container.innerHTML = `
-  <div class="form-box" id="form">
-  <h1 id="title">Login</h1>
-  <form id="loginForm">
-    <div class="input-field" id="namefield">
-      <i class="fa-solid fa-user"></i>
-      <input
-        type="text"
-        placeholder="Username"
-        id="username"
-        name="username"
-        autocomplete="off"
-      />
-    </div>
-    <div class="input-field">
-      <i class="fa-solid fa-lock"></i>
-      <input
-        type="password"
-        placeholder="Password"
-        id="password"
-        name="password"
-      />
-    </div>
-    <div class="btn-field">
-      <button type="button" id="loginBtn">Login</button>
-    </div>
-  </form>
-  <p class="Login">
-    Don't have an account? <a href="index.html"> Register</a>
-  </p>
-</div>
-            `;
-  document.body.appendChild(container);
 }
