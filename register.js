96055af8be9b1da6777eeb77ed7bf1e8d786f421
suryaprakash2805 <suryaprakashraj28@gmail.com>
2024-04-13 18:56:07 +0530

    commit js

--- register.js
+++ register.js
@@ -54,3 +54,63 @@ document.addEventListener("DOMContentLoaded", function () {
     window.location.href = "login.html";
   });
 });
+
+const bgImage = new Image();
+bgImage.src = "background.jpg";
+bgImage.onload = function () {
+  // Once the image is loaded, add it to the body as the background
+  document.body.style.backgroundImage = "url('background.jpg')";
+  // Now load the rest of the HTML content
+  loadHTMLContent();
+};
+
+function loadHTMLContent() {
+  // Here you can add your HTML content dynamically
+  const container = document.createElement("div");
+  // container.className = "container";
+  // Add your HTML content here
+  container.innerHTML = `
+  <div class="form-box" id="form">
+  <h1 id="title">Register</h1>
+  <form class="input-group" id="registerForm">
+    <div class="input-field" id="namefield">
+      <i class="fa-solid fa-user"></i>
+      <input
+        type="text"
+        placeholder="Username"
+        id="username"
+        autocomplete="off"
+      />
+    </div>
+    <div class="input-field">
+      <i class="fa-solid fa-envelope"></i>
+      <input
+        type="email"
+        placeholder="Email"
+        id="email"
+        autocomplete="off"
+      />
+    </div>
+    <div class="input-field">
+      <i class="fa-solid fa-lock"></i>
+      <input type="password" placeholder="Password" id="password" />
+    </div>
+    <div class="btn-field">
+      <button type="button" id="registerBtn">Register</button>
+    </div>
+  </form>
+  <p class="Login">
+    Already have an account?<a href="login.html"> Login</a>
+  </p>
+  <footer>
+    © Copyright by
+    <a
+      href="https://www.linkedin.com/in/suryaprakash2805"
+      class="copyright"
+      >Surya Prakash</a
+    >
+  </footer>
+</div>
+            `;
+  document.body.appendChild(container);
+}
